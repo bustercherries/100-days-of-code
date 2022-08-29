fc5bfaa93aaffffc42ff75b80064bcfe9905ec18
Róża <jackowska.roza23@gmail.com>
2022-08-29 19:14:57 +0200

    Update tipcalculator.py

--- 100 days of code/tipcalculator.py
+++ 100 days of code/tipcalculator.py
@@ -1,8 +1,8 @@
 print("Welcome to the tip calculator.")
-bill = input("What was the total bill? ")
+bill = input("What was the total bill? $")
 tip = input("What percentage tip would you like to give? 10, 12 or 15? ")
 no_people = input("How many people to split the bill? ")
 
 x = ((float(bill) * float(tip)/100) + float(bill))/ int(no_people)
 
-print(f'Each person should pay: {round(x, 2)}')
+print(f'Each person should pay: ${round(x, 2)}')
