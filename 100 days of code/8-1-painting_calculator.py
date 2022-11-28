3067b90729ec4c54a01175eb7a0ff41f414ceb2f
Róża <jackowska.roza23@gmail.com>
2022-11-28 19:20:51 +0100

    using ceil function

--- 100 days of code/8-1-painting_calculator.py
+++ 100 days of code/8-1-painting_calculator.py
@@ -1,10 +1,11 @@
+import math
 
 test_h = int(input("Height of the wall: "))
 test_w = int(input("Width of the wall: "))
 coverage = 5
 
 def calculator(height = test_h, width = test_w, cov = coverage):
-    number_of_cans = round((height*width)/cov)
-    print(f"The number of cans: {number_of_cans}")
+    number_of_cans = (height*width)/cov
+    print(f"The number of cans: {math.ceil(number_of_cans)}")
 
 calculator()
