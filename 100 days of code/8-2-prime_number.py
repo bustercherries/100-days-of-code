5a1b034d5810dbe55fd9c44f71c5a5da885158b7
Róża <jackowska.roza23@gmail.com>
2022-11-28 19:54:57 +0100

    new

---
+++ 100 days of code/8-2-prime_number.py
@@ -0,0 +1,21 @@
+#Write your code below this line 👇
+
+def prime_checker(number):
+    is_prime = True
+    for x in range(2, number):
+        if number % x == 0:
+            is_prime = False
+
+    if is_prime == False:
+        print("It's not a prime number.")
+    elif is_prime == True:
+        print("It's a prime number.")
+
+
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
