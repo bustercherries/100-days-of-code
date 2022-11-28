ba120c9dbba932ce01f529b85d548d3c4b1ffe13
Róża <jackowska.roza23@gmail.com>
2022-11-28 19:16:26 +0100

    modified printing

--- 100 days of code/8-1-painting_calculator.py
+++ 100 days of code/8-1-painting_calculator.py
@@ -5,6 +5,6 @@ coverage = 5
 
 def calculator(height = test_h, width = test_w, cov = coverage):
     number_of_cans = round((height*width)/cov)
-    print(number_of_cans)
+    print(f"The number of cans: {number_of_cans}")
 
 calculator()
