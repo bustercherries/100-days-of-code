17de6460f5774fcf598451322c1a544b33102c00
Róża <jackowska.roza23@gmail.com>
2022-12-06 16:45:29 +0100

    new

--- 100 days of code/8-ceaser_cipher.py
+++ 100 days of code/8-ceaser_cipher.py
@@ -1,18 +1,20 @@
+import math
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
 def ceasar(c_direction, c_text, c_shift):
     cipher = ""
     index = 0
     for letter in c_text:
         index = alphabet.index(letter)
         if c_direction == "decode":
+            math.fabs(c_shift)
             c_shift *= -1
         cipher += alphabet[index + c_shift]
     print(f"The {c_direction}d text is {cipher}")
 
-ceasar(direction, text, shift)
+ceasar(c_direction=direction, c_text=text, c_shift=shift)
