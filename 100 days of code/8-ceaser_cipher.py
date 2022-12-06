d76ed08667bf59df13ae08aef00b6b09813278fb
Róża <jackowska.roza23@gmail.com>
2022-12-06 18:45:00 +0100

    final project

--- 100 days of code/8-ceaser_cipher.py
+++ 100 days of code/8-ceaser_cipher.py
@@ -1,20 +1,35 @@
 import math
+import art
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 def ceasar(c_direction, c_text, c_shift):
     cipher = ""
-    index = 0
-    for letter in c_text:
-        index = alphabet.index(letter)
-        if c_direction == "decode":
-            math.fabs(c_shift)
+    x = 0
+    if c_direction == "decode":
             c_shift *= -1
-        cipher += alphabet[index + c_shift]
+    for char in c_text:
+        if char in alphabet:
+            x = alphabet.index(char)
+            cipher += alphabet[x + c_shift]
+        else:
+            cipher += char
     print(f"The {c_direction}d text is {cipher}")
 
-ceasar(c_direction=direction, c_text=text, c_shift=shift)
+print(art.logo)
+ 
+should_countine = True
+while should_countine:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    ceasar(c_direction=direction, c_text=text, c_shift=shift)
+
+    shift %= len(alphabet) 
+    answer = input("Type 'yes' if you want to go again. Otherwise type 'no'.")
+    if answer == 'no':
+        should_countine = False
+        print("Goodbye!")
+
+
