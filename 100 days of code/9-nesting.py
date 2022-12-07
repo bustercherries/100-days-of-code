c1fbba588d6ac4b69af7bbd4844131ba54baba6b
Róża <jackowska.roza23@gmail.com>
2022-12-07 18:27:40 +0100

    dictionaries

---
+++ 100 days of code/9-nesting.py
@@ -0,0 +1,26 @@
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above 
+#First *fork* your copy. Then copy-paste your code below this line 👇
+#Finally click "Run" to execute the tests
+
+def add_new_country(country, visits, cities):
+    new_dictionary = {}
+    new_dictionary["country"] = country
+    new_dictionary["visits"] = visits
+    new_dictionary["cities"] = cities
+    travel_log.append(new_dictionary)
+
+#🚨 Do NOT change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
