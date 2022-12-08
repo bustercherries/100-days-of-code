47bf3afa062437edae1755ff228b6210eeab5114
Róża <jackowska.roza23@gmail.com>
2022-12-08 21:41:52 +0100

    final

--- 100 days of code/9-blind_bid.py
+++ 100 days of code/9-blind_bid.py
@@ -10,9 +10,9 @@ def highest_bidder(bidding_record):
     highest_amount = 0
     winner = ""
     for bidder in bidding_record:
-        bid_amount = bidding_record[bidder]
-        if int(bid_amount) > highest_amount:
-            highest_amount == int(bid_amount)
+        bid_amount = int(bidding_record[bidder])
+        if bid_amount > highest_amount:
+            highest_amount == bid_amount
             winner = bidder
     print(f"The winner is {winner} with the amount ${highest_amount}.")
 
